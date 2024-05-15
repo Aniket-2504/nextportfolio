b272ce51949cf2966da135ea51f519ceffa23ea1
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-05-15 19:59:41 +0530

    Hero section Done

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,8 +1,59 @@
+import { MovingBorderBtn } from '@/components/ui/moving-border'
+import Link from 'next/link'
 import React from 'react'
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className='min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between'> 
+         
+
+        <div className='space-y-10 text-center lg:text-left'>
+
+                <h1 className='text-4xl lg:text-7xl font-bold'> Nice to meet you  
+                    <br />{" "}
+                  <span className='underline underline-offset-8 decoration-green-500 hover:text-green-400 cursor-pointer'> 
+                      {"I'm Aniket."}
+                  </span> 
+                </h1>
+
+            
+                <p className='md:w-96 text-lg text-gray-300'>
+                    {
+                    "I am Aniket a 19 year cs graduate passionate about developement and design, And I love to make things, Apart from that I do freelancing in design."
+                    }
+                    </p>
+            
+            <Link href={"mailto:aniketdhaygude@gmail.com"} className='inline-block group'>
+                <div>
+                    <h1 className='text-3xl font-bold group-hover:text-green-400 transition-all'>Contact me 📬</h1>
+                    <div className='w-40 h-2 bg-green-500 rounded-full'></div>
+                    <div className='w-40 h-2 bg-indigo-500 rounded-full translate-x-2'></div>
+                </div>
+            </Link>
+        </div>
+
+        <div className='relative'>  
+
+            <div className='w-72 h-72 space-y-3 -rotate-[30deg] relative'>
+                    <div className='flex gap-3 translate-x-8'>
+                        <div className='w-32 h-32 rounded-2xl bg-indigo-500 hover:bg-orange-500'></div>
+                        <div className='w-32 h-32 rounded-full bg-green-500 hover:bg-red-500'></div>
+                    </div>
+                    <div className='flex gap-3 -translate-x-8'>
+                        <div className='w-32 h-32 rounded-2xl bg-gray-300 hover:bg-green-500'></div>
+                        <div className='w-32 h-32 rounded-full bg-indigo-500 hover:bg-yellow-500'></div>
+                    </div>
+                    <div className=' glow absolute top-[40%] right-1/2 -z-10'> </div>
+            </div> 
+
+            <div className='absolute bottom-5 sm:bottom-14 left-0 sm:-left-10'>
+              <MovingBorderBtn borderRadius='0.5rem' className='p-3 font-semibold'> 
+                <p>Available for Work</p>
+                </MovingBorderBtn>
+                </div>
+        </div> 
+
+    </div>
   )
 }
 
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function page() {
   return (
-    <div className='min-h-screen bg-black'>
-      
-     <div className='max-w-7xl mx-auto'>
+      <div className='min-h-screen bg-black overflow-hidden'>
 
-     
-<Navbar/>
+        <div className='dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative'>
+
+       
+        
+        <div className='max-w-7xl mx-auto p-5 '>
+
+              
+            <Navbar/>   
+
+            <HeroSection/>
 
 
-     </div>
+        </div>
+        <div className='h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full'>
+
+        </div>
 
 
-    </div>
+        </div>
+
+      </div>
   )
 }
 
